0dd25892edc9a14549a87ba1789f224a363b3f4d
Marine Deville <44803422+Xelfie@users.noreply.github.com>
2021-05-29 01:12:54 +0400

    back to full axios api

--- src/api/index.js
+++ src/api/index.js
@@ -1,28 +1,15 @@
 import axios from 'axios';
-import Amplify, { API } from 'aws-amplify';
-
-Amplify.configure({
-    API: {
-        endpoints: [
-            {
-                name: "name",
-                endpoint: "https://api.guillaumeblackburn.me",
-
-            }
-        ]
-    }
-});
 
 // set base URL
-const API2 = axios.create({ baseURL: 'https://api.guillaumeblackburn.me', withCredentials: true });
+const API = axios.create({ baseURL: 'https://api.guillaumeblackburn.me' });
 
-// // api calls
-export const login = (user) => API.post('name', '/user/login', {body: user} );
-export const registerStaff = (newUser) => API2.post('/user/register-staff', newUser);
-export const logout = () => API2.get('/user/logout');
-export const listStaff = () => API2.get('/user/list-staff');
-export const addMenuCategory = (name) => API2.post('/menu/add-menu-category', name);
-export const addMenuItem = (menuItem) => API2.post('/menu/add-menu-item', menuItem, {
+// api calls
+export const login = (user) => API.post('/user/login', user);
+export const registerStaff = (newUser) => API.post('/user/register-staff', newUser);
+export const logout = () => API.get('/user/logout');
+export const listStaff = () => API.get('/user/list-staff');
+export const addMenuCategory = (name) => API.post('/menu/add-menu-category', name);
+export const addMenuItem = (menuItem) => API.post('/menu/add-menu-item', menuItem, {
     headers: {
         "Content-Type": "multipart/form-data",
     },
