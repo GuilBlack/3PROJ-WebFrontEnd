0f6aca0e6d48fae4ebccef9f6da3bdc1db35adb8
Marine Deville <44803422+Xelfie@users.noreply.github.com>
2021-05-28 22:18:54 +0400

    Updated API

--- src/api/index.js
+++ src/api/index.js
@@ -1,10 +1,16 @@
 import axios from 'axios';
 
 // set base URL
-const API = axios.create({ baseURL: 'http://localhost:6969', withCredentials: true });
+const API = axios.create({ baseURL: 'https://api.guillaumeblackburn.me', withCredentials: true });
 
 // api calls
 export const login = (user) => API.post('/user/login', user);
 export const registerStaff = (newUser) => API.post('/user/register-staff', newUser);
 export const logout = () => API.get('/user/logout');
 export const listStaff = () => API.get('/user/list-staff');
+export const addMenuCategory = (name) => API.post('/menu/add-menu-category', name);
+export const addMenuItem = (menuItem) => API.post('/menu/add-menu-item', menuItem, {
+    headers: {
+        "Content-Type": "multipart/form-data",
+    },
+});
