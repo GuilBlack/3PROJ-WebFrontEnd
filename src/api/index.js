4eebf507674ff7696d024ad9101d24af76e9aece
Marine Deville <44803422+Xelfie@users.noreply.github.com>
2021-05-29 16:34:41 +0400

    proxy test for deployed app

--- src/api/index.js
+++ src/api/index.js
@@ -1,15 +1,12 @@
 import axios from 'axios';
 
-// set base URL
-const API = axios.create({ baseURL: 'https://api.guillaumeblackburn.me' });
-
-// api calls
-export const login = (user) => API.post('/user/login', user);
-export const registerStaff = (newUser) => API.post('/user/register-staff', newUser);
-export const logout = () => API.get('/user/logout');
-export const listStaff = () => API.get('/user/list-staff');
-export const addMenuCategory = (name) => API.post('/menu/add-menu-category', name);
-export const addMenuItem = (menuItem) => API.post('/menu/add-menu-item', menuItem, {
+// axios calls
+export const login = (user) => axios.post('/user/login', user);
+export const registerStaff = (newUser) => axios.post('/user/register-staff', newUser);
+export const logout = () => axios.get('/user/logout');
+export const listStaff = () => axios.get('/user/list-staff');
+export const addMenuCategory = (name) => axios.post('/menu/add-menu-category', name);
+export const addMenuItem = (menuItem) => axios.post('/menu/add-menu-item', menuItem, {
     headers: {
         "Content-Type": "multipart/form-data",
     },
