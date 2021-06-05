821715025dbc12331a138080dac2ae607fe872ce
Marine Deville <44803422+Xelfie@users.noreply.github.com>
2021-06-05 21:20:08 +0400

    Added warning for when the user wants to update the table layout before 11PM

--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { Button, Alert } from 'react-bootstrap';
+import Popup from 'reactjs-popup';
+import 'reactjs-popup/dist/index.css';
 import MapTile from './MapTile';
 import Spinner from '../Spinner';
 import { getTableArrangement, updateTableArrangement } from '../../api';
@@ -11,11 +13,7 @@ export default function Map() {
     const [info, setInfo] = useState(false);
     const [success, setSuccess] = useState(false);
     const [err, setErr] = useState();
-
-    /**
-     * TODO:
-     * - success msgs for saving layout
-     */
+    const [openModal, setOpenModal] = useState(false);
 
     useEffect(() => {
         // api call
@@ -40,27 +38,34 @@ export default function Map() {
                 }
                 setLoading(false);
             })
-    }, []);
-
-    const updateMapLayout = () => {
-        setInfo(true);
-        updateTableArrangement({ layout: mapLayout })
-            .then((res) => {
-                console.log(res);
-                setSuccess(true);
-                setErr();
-            })
-            .catch((err) => {
-                if (err.response) {
-                    if (err.response.status === 401)
+        }, []);
+        
+        const updateMapLayout = (canProceed) => {
+            let today = new Date();
+            let currTime = today.getHours();
+            
+            // check if the user can update the table arrangement at the current time
+            if (currTime >= 23 || canProceed) {
+                setInfo(true);
+                updateTableArrangement({ layout: mapLayout })
+                .then((res) => {
+                    setSuccess(true);
+                    setErr();
+                })
+                .catch((err) => {
+                    if (err.response) {
+                        if (err.response.status === 401)
                         setErr("An unexpected error occured. Please log out and log back in to proceed.");
-                    else
+                        else
                         setErr(err.response.data.message);
-                } else {
-                    setErr("Failed to update the map: our servers are down at the moment.");
-                }
-            });
-        setInfo(false);
+                    } else {
+                        setErr("Failed to update the map: our servers are down at the moment.");
+                    }
+                });
+                setInfo(false);
+        } else {
+            setOpenModal(true);
+        }
     }
 
     // make success alert automatically go away after 2 seconds
@@ -69,8 +74,6 @@ export default function Map() {
             setSuccess(false);
         }, 2000);
     }
-
-    console.log(mapLayout);
 
     return (
         <div>
@@ -106,7 +109,7 @@ export default function Map() {
                                         mapLayout?.map(mapTile => (
                                             mapTile.position === 9 || mapTile.position === 16 
                                             ? <>
-                                            <div></div> 
+                                            <div key={(mapTile.position + 30)}></div> 
                                             <MapTile tile={mapTile} key={mapTile.position} mapLayout={mapLayout} setMapLayout={setMapLayout} />
                                             </>
                                             : <MapTile tile={mapTile} key={mapTile.position} mapLayout={mapLayout} setMapLayout={setMapLayout} />
@@ -115,11 +118,42 @@ export default function Map() {
                                 </div>
                         }
                     </div>
-                    <Button id="btn-map-save" variant="info" onClick={() => updateMapLayout()}> Save Layout </Button>
+                    <Button id="btn-map-save" variant="info" onClick={() => updateMapLayout(false)}> Save Layout </Button>
                 </div>                    
             </div>
 
-
+            <Popup 
+                open={openModal}
+                closeOnDocumentClick
+                onClose={() => setOpenModal(false)}
+                modal
+            >
+                <div className="text-center popup-info">
+                    <i class="bi bi-exclamation-triangle-fill text-danger" style={{fontSize:"10em"}}></i>
+                    <p>
+                        You're about to save a layout while operations might still be going on in the restaurant for the day.{' '}
+                        <strong className="text-danger">Proceed only if the restaurant is currently not in activity.</strong>
+                    </p>
+                    <br />
+                    <div className="d-flex justify-content-around">
+                        <Button
+                            variant="outline-danger"
+                            onClick={() => {
+                                updateMapLayout(true); 
+                                setOpenModal(false);
+                            }}
+                        >
+                            Proceed
+                        </Button>
+                        <Button
+                            variant="secondary"
+                            onClick={() => setOpenModal(false)}
+                        >
+                            Cancel
+                        </Button>
+                    </div>
+                </div>
+            </Popup>
         </div>
     )
 }
