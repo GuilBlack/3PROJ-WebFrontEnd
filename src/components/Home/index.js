1d03608f9648dbe6d7f5d59d6a5506caa0a8ff22
Marine Deville <44803422+Xelfie@users.noreply.github.com>
2021-06-05 21:20:36 +0400

    Better design

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -5,9 +5,50 @@ export default function Home() {
     return (
         <div className="text-center">
             <h1 className="text-center">Welcome!</h1>
-            <Link to="/staff">View Staff</Link> <br />
-            <Link to="/register-staff">Register new staff member</Link> <br />
-            <Link to="/menu">Edit menu</Link>
+            <div id="home-dashboard">
+                <Link to="/staff">
+                    <div className="home-item">
+                        <i className="bi bi-person-fill"></i>
+                        <br />
+                        <h2>Staff</h2>
+                    </div>
+                </Link>
+                <Link to="/map">
+                    <div className="home-item">
+                        {/* <i className="bi bi-ui-checks-grid"></i> */}
+                        {/* <i className="bi bi-map"></i> */}
+                        <i className="bi bi-building"></i>
+                        <br />
+                        <h2>Map</h2>
+                    </div>
+                </Link>
+                <Link to="/menu">
+                    <div className="home-item">
+                        <i className="bi bi-layout-text-sidebar-reverse"></i>
+                        {/* <i className="bi bi-menu-app-fill"></i> */}
+                        <br />
+                        <h2>Menu</h2>
+                    </div>
+                </Link>
+                <Link to="/stock">
+                    <div className="home-item">
+                        <i className="bi bi-box-seam"></i>
+                        <br />
+                        <h2>Stock</h2>
+                    </div>
+                </Link>
+                <Link to="/sales">
+                    <div className="home-item">
+                        {/* <i className="bi bi-currency-euro"></i> */}
+                        {/* <i className="bi bi-wallet2"></i> */}
+                        {/* <i className="bi bi-credit-card"></i> */}
+                        <i className="bi bi-receipt-cutoff"></i>
+                        <br />
+                        <h2>Sales</h2>
+                    </div>
+                </Link>
+            </div>
+
         </div>
     );
 }
