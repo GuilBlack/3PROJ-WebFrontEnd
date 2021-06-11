319e1d955daf408ba37a3466673daf06e70554df
Guillaume Blackburn <guillaume.blackburn1@gmail.com>
2021-06-11 14:19:30 +0400

    deploying the app on amplify

--- src/api/index.js
+++ src/api/index.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // set base URL
 const API = axios.create({
-	baseURL: "http://localhost:6969",
+	baseURL: "https://api.guillaumeblackburn.me",
 	withCredentials: true,
 });
 
