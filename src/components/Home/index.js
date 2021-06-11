e898128d9a623dd1b7a8fbf204112bc8cc8e7303
Marine Deville <44803422+Xelfie@users.noreply.github.com>
2021-06-12 02:37:15 +0400

    Code cleanup

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -15,8 +15,6 @@ export default function Home() {
                 </Link>
                 <Link to="/map">
                     <div className="home-item">
-                        {/* <i className="bi bi-ui-checks-grid"></i> */}
-                        {/* <i className="bi bi-map"></i> */}
                         <i className="bi bi-building"></i>
                         <br />
                         <h2>Map</h2>
@@ -25,7 +23,6 @@ export default function Home() {
                 <Link to="/menu">
                     <div className="home-item">
                         <i className="bi bi-layout-text-sidebar-reverse"></i>
-                        {/* <i className="bi bi-menu-app-fill"></i> */}
                         <br />
                         <h2>Menu</h2>
                     </div>
@@ -39,9 +36,6 @@ export default function Home() {
                 </Link>
                 <Link to="/sales">
                     <div className="home-item">
-                        {/* <i className="bi bi-currency-euro"></i> */}
-                        {/* <i className="bi bi-wallet2"></i> */}
-                        {/* <i className="bi bi-credit-card"></i> */}
                         <i className="bi bi-receipt-cutoff"></i>
                         <br />
                         <h2>Sales</h2>
